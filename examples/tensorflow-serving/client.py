70dfc53ec51225cbc49a7b9ed7d8bdb3a0f276cb
Marc Kohaupt <debuglevel@gmail.com>
2020-04-03 23:39:13 +0200

    refactoring

--- examples/tensorflow-serving/client.py
+++ examples/tensorflow-serving/client.py
@@ -6,44 +6,40 @@ import numpy as np
 import argparse
 import time
 
-def read_image_as_tensor(filename, height, width, tensorflow_backend="builtin"):
+def get_picture_as_tensor(filename, height, width, tensorflow_backend="builtin"):
   if tensorflow_backend == "rest":
-    return read_image_as_tensor_rest(filename, height, width)
+    retuget_pictureage_as_tensor_rest(filename, height, width)
   elif tensorflow_backend == "builtin":
-    return read_image_as_tensor_builtin(filename, height, width)
+    return get_picture_as_tensor_builtin(filename, height, width)
 
-def read_image_as_tensor_rest(filename, height, width):
+def get_picture_as_tensor_rest(filename, height, width):
   url = f'http://localhost:5000/v1/pictures/?height={height}&width={width}'
   data = open(filename, "rb").read()
   response = requests.post(url, data=data)
   json_response = response.json()
   return json_response["tensor"]
 
-def read_image_as_tensor_builtin(filename, height, width):
+def get_picture_as_tensor_builtin(filename, height, width):
   # importing keras/tensorflow is quite slow; only do it if needed
   from keras.preprocessing import image
 
-  # CAVEAT: height and width might be switched; did not verify.
-  img = image.load_img(filename, target_size=(height, width))
-  img_array = image.img_to_array(img) / 255.
+  img_pil = image.load_img(filename, target_size=(height, width))
+  img_array = image.img_to_array(img_pil) / 255.
   img_float = img_array.astype('float16')
   img_list = img_float.tolist()
   return img_list
 
-def read_image(filename):
-  return Image.open(filename)
-
-def build_json(tensor):
+def build_tensorflowserving_payload(tensor):
   data = json.dumps(
     {
       "signature_name": "serving_default",
       "instances": [{'input_1': tensor}]
      }
     )
-
   return data
 
-def send_request(json_data, model_name, server):
+def classify_tensor(tensor, model_name, server):
+  json_data = build_tensorflowserving_payload(tensor)
   headers = {"content-type": "application/json"}
   json_response = requests.post(f'{server}/v1/models/{model_name}:predict', data=json_data, headers=headers)
   predictions = json.loads(json_response.text)['predictions']
@@ -90,9 +86,8 @@ def main():
   if print_model_details:
     print_model_details(model_name="questionnaire", server=server)
 
-  tensor = read_image_as_tensor(filename, height=224, width=224, tensorflow_backend=tensorflow_backend)
-  json = build_json(tensor)
-  send_request(json, model_name="questionnaire", server=server)
+  tensor = get_picture_as_tensor(filename, height=224, width=224, tensorflow_backend=tensorflow_backend)
+  classify_tensor(tensor, model_name="questionnaire", server=server)
 
 if __name__ == '__main__':
   main()
